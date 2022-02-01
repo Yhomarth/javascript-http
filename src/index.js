57ecb8d617cb247e33347134475790f77b3a22ce
Yhomarth <yhomarth@gmail.com>
2022-02-01 11:19:16 -0400

    Clase 139 completada, Peticion HTTP - Fetech - Get

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 
-import './styles.css';
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
+fetch(jokeUrl).then( resp => {
+    
+    resp.json().then(  ({id, value}) => {
+
+        console.log(id);
+        console.log(value);
+        
+    })
+
+} );
